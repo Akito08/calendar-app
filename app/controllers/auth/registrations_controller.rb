8488693100b48bc7dff0e5a3bd22b11c9b7aef27
Akito08 <sza9ki8to7@gmail.com>
2023-10-10 22:58:50 +0900

    フロントの新規登録とログイン画面　完成

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -1,4 +1,14 @@
 class Auth::RegistrationsController < ApplicationController
+  def create
+    user = User.new(sign_up_params)
+    if user.save
+      render json: { status: "ユーザー登録が完了しました" }, status: 201
+    else
+      render json: { errors: user.errors.full_messages }, status: 422
+    end
+  end
+
+
   private
 
   def sign_up_params
